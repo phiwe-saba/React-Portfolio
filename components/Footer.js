6fb89656e8a9820c1f55f0972d26d74d1f6d58f4
phiwe saba <sabasbulele81@gmail.com>
2022-06-21 17:49:19 +0200

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@
 function footer(){
     return(
         <div className="w-full h-16 bg-gray-50 flex justify-center items-center">
-            <h1 className="text-xl text-blue-600 mr-2">Phiwe Saba</h1>
+            <h1 className="text-xl text-blue-600 mr-2">Sibulele Saba</h1>
         </div>
     );
 }
