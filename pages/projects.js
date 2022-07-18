cc3434f5f5c03379df49942260b4f66963c25bc3
Phiwe Sibulele Saba <74997133+phiwe-saba@users.noreply.github.com>
2022-07-18 09:46:22 +0200

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -12,7 +12,7 @@ function projects(){
                         <p className="text-gray-700 text-base mb-4">
                             Website to search and apply for jobs.
                         </p>
-                        <a href="https://github.com/phiwe-saba/We-Reccruit-Application" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                        <a href="https://github.com/phiwe-saba/recruitment-app" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                                 View Project.
                         </a>
                     </div>
