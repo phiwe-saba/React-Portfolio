57d06ca89181ab56b88d8a8d417200f8607cddc0
Phiwe Sibulele Saba <74997133+phiwe-saba@users.noreply.github.com>
2022-07-18 09:39:29 +0200

    Update homepage.js

--- pages/homepage.js
+++ pages/homepage.js
@@ -15,11 +15,7 @@ function Home(){
                             Hello, <span className="text-blue-600">Sibulele</span> here!
                         </h1>
                         <p className="text-left font-normal font mb-5 flex-wrap">
-                            I am seeking to pursue opportunities with organizations that will enhance my knowledge and skills as a Software Developer.
-                            And SovTech is the right company to assist me in achieving the goal that I want to reach. I would like to be
-                            a software developer at SovTech because I want to be part of the team that is changing the way businesses across the 
-                            world build software. I believe after you have reviewed my CV you will see that I have acquired the essential skills and qualifications
-                            which match your requirements.
+                            I am a talented graduate developer with a strong academic background seeking a position that incorporates problem-solving, critical thinking and expands and improves on current skills. Expertise of building web applications and websites using C#, .Net technologies, Laravel, HTML, CSS and JavaScript.
                         </p>
                         <a href="https://drive.google.com/file/d/1Hcpf9GM0RXg0ocC9Zkbu2XPCtOp8wNsj/view?usp=sharing" className="font-semibold text-white md:mt-10 mt-5 pt-5 pb-10 bg-blue-600 rounded-md w-60 h-16 text-lg hover:bg-black">
                             Download CV.
